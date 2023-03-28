52b15bb27ccc0acd0693be4cf2f580be4eada539
kubracennet <kubracennetyavasoglu@gmail.com>
2023-03-28 22:10:14 +0300

    Initial Commit

---
+++ AppleStore/AppleStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStoreApp.swift
+//  AppleStore
+//
+//  Created by Kübra Cennet Yavaşoğlu on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
